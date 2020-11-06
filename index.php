9ccd86074732ad827561e5dc84f626d862c5d9bc
Gabriela Vespoli <gabiat3@hotmail.com>
2020-11-06 19:15:22 -0300

    Projeto Prático Bootstrap

--- index.php
+++ index.php
@@ -5,15 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Full Stack E-commerce</title>
     <link rel="stylesheet" href="./css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
     <!-- Menu -->
     <?php include_once('menu.html');
 ?>
-    <br><br><br><br>
-    <br><br><br><br>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Seja bem vindo(a)!</h1>
+      <p class="lead">Aqui em nossa loja, <em>programadores tem desconto</em> nos produtos para a sua casa!</p>
+    </div>
+  </div>
 
-    <main>
+  <div class="container d-flex justify-content-center align-items-center">
+  <div class="row">
+    <div class="col-sm foto">
+      <img width="100%" height="100%" src="./img/img_bv.jpg" alt="Imagem ilustrando compras na era digital">
+    </div>
+    <div class="col-sm foto">
+      <img width="100%" height="100%" src="./img/img_bv2.jpg" alt="Imagem ilustrando compras na era digital">
+    </div>
+    <div class="col-sm foto">
+      <img width="100%" height="100%" src="./img/img_bv3.jpg" alt="Imagem ilustrando compras na era digital">
+    </div>
+  </div>
+</div>
+
+    <!-- <br><br><br><br> -->
+
+    <!-- <main>
       <h2>Seja bem vindo(a)!</h2>
 
       <p>Aqui em nossa loja, <em>programadores tem desconto</em> nos produtos para a sua casa!</p>
@@ -21,14 +42,15 @@
         <div class="foto">
           <img src="./img/img_bv.jpg" alt="Imagem ilustrando compras na era digital">
         </div>
-    </main>
+    </main> -->
 
-    <footer id="rodape">
-      <p id="formas_pagamento"><b>Formas de pagamento</b></p>
-      <img src="./img/Formasdepagamento.png" alt="Formas de pagamento" />
-      <br><br><br><br>
-      <p>&copy; 2020  Recode Pro · Projeto Prático CSS Básico · E-commerce by Gabi Vespoli</p>
-    </footer>
+    <!-- Rodapé -->
+    <?php include_once('footer.html')
+    ?>
+
     <script src="./js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
--- lojas.php
+++ lojas.php
@@ -5,21 +5,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Lojas - Full Stack E-commerce</title>
     <link rel="stylesheet" href="./css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
     <!-- Menu -->
     <?php include_once('menu.html');
 ?>
-    <br><br><br><br>
-    <br><br><br><br>
+    <!-- <br><br><br><br> -->
+    <!-- <br><br><br><br> -->
 
-    <main>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Lojas</h1>
+        <p class="lead">Conheça nossas lojas.</p>
+      </div>
+    </div>
+
+    <!-- <main>
     <header>
       <h2>Nossas Lojas</h2>
     </header>
-    <hr />
+    <hr /> -->
 
-    <section class="lojas">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h2>São Paulo</h2>
+          <p>Av. Paulista, 2000</p>
+          <p>10 &ordm; andar</p>
+          <p>Jardins</p>
+          <p>(11) 2222-2222</p>
+          <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Rio de Janeiro</h2>
+          <p>Av. Copacabana, 2300</p>
+          <p>5 &ordm; andar</p>
+          <p>Copacabana</p>
+          <p>(21) 2121-3333</p>
+          <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Floripa</h2>
+          <p>Av. Beira Mar, 200</p>
+          <p>1 &ordm; andar</p>
+          <p>Beira Mar</p>
+          <p>(48) 2222-0000</p>
+          <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes &raquo;</a></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- <section class="lojas">
           <div class="loja_box">
               <h3>São Paulo</h3>
               <p>Av. Paulista, 2000</p>
@@ -44,17 +81,17 @@
     </section>
     <section class="galeria">
       <img src="./img/img_lojas.png" alt="Fotos de São Paulo, Rio de Janeiro e Florianópolis">
-    </section>
+    </section> -->
 
-  </main>
+  <!-- </main> -->
+    <!-- <br><br><br><br> -->
 
-    <br><br><br><br>
-    <footer id="rodape">
-      <p id="formas_pagamento"><b>Formas de pagamento</b></p>
-      <img src="./img/Formasdepagamento.png" alt="Formas de pagamento" />
-      <br><br><br><br>
-      <p>&copy; 2020  Recode Pro · Projeto Prático CSS Básico · E-commerce by Gabi Vespoli</p>
-    </footer>
+    <!-- Rodapé -->
+    <?php include_once('footer.html')
+    ?>
     <script src="./js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
